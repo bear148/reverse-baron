8e64d0ca999002589b6f5464325999314d9cb8c0
bear148 <bizzeebear@hotmail.com>
2025-01-31 13:54:47 -0800

    Reorder file

--- wordtwist/wordtwist.puzzlebaron.com/js/md5.js
+++ wordtwist/wordtwist.puzzlebaron.com/js/md5.js
@@ -54,6 +54,50 @@ function wordHandle(data, files) {
     }
 }
 
+function readFile(file) {
+    return new Promise(function(resolve, reject) {
+        let fr = new FileReader();
+
+        fr.onload = function() {
+            resolve(fr.result);
+        }
+
+        fr.onerror = function() {
+            resolve(fr);
+        }
+
+        fr.readAsText(file);
+    })
+}
+
+// md5Files is an array where the first value in the array is the md5File containing all of the md5Codes, and the
+// second value in the array is the file with all the words and corresponding line number for the MD5 code.
+function reverseLookupMD5(md5, md5Files) {
+    let readers = [];
+    //let out = document.getElementById("md5_reversed");
+
+    for(let i = 0;i < md5Files.length;i++){
+        readers.push(readFile(md5Files[i]));
+    }
+
+    Promise.all(readers).then((values) => {
+        let a_md5 = values[0].split(/\r?\n|\r|\n/g);
+        let loc = 0; // LOC is the line number starting at 0, so the actual number is loc + 1
+        for (let i = 0; i < a_md5.length; i++) {
+            if (a_md5[i] == md5) {
+                loc = i;
+                break
+            }
+        }
+
+        let a_w = values[1].split(/\r?\n|\r|\n/g);
+        $('#word').val(a_w[loc]);
+        $("#tmpgame").submit();
+        console.log(`Word: ${a_w[loc]} | Line: ${loc+1}`);
+    });
+}
+
+/* PUZZLE BARON SPECIFIC CODE */
 var MD5 = function (string) {
 
     function RotateLeft(lValue, iShiftBits) {
@@ -253,47 +297,4 @@ var MD5 = function (string) {
     var temp = WordToHex(a)+WordToHex(b)+WordToHex(c)+WordToHex(d);
 
     return temp.toLowerCase();
-}
-
-function readFile(file) {
-    return new Promise(function(resolve, reject) {
-        let fr = new FileReader();
-
-        fr.onload = function() {
-            resolve(fr.result);
-        }
-
-        fr.onerror = function() {
-            resolve(fr);
-        }
-
-        fr.readAsText(file);
-    })
-}
-
-// md5Files is an array where the first value in the array is the md5File containing all of the md5Codes, and the
-// second value in the array is the file with all the words and corresponding line number for the MD5 code.
-function reverseLookupMD5(md5, md5Files) {
-    let readers = [];
-    //let out = document.getElementById("md5_reversed");
-
-    for(let i = 0;i < md5Files.length;i++){
-        readers.push(readFile(md5Files[i]));
-    }
-
-    Promise.all(readers).then((values) => {
-        let a_md5 = values[0].split(/\r?\n|\r|\n/g);
-        let loc = 0; // LOC is the line number starting at 0, so the actual number is loc + 1
-        for (let i = 0; i < a_md5.length; i++) {
-            if (a_md5[i] == md5) {
-                loc = i;
-                break
-            }
-        }
-
-        let a_w = values[1].split(/\r?\n|\r|\n/g);
-        $('#word').val(a_w[loc]);
-        $("#tmpgame").submit();
-        console.log(`Word: ${a_w[loc]} | Line: ${loc+1}`);
-    });
 }
